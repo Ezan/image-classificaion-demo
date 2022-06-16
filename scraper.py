42ed1f5e749c648dafe41701b2c8b65cf5f5a186
Ejan <ezan.shakya@gmail.com>
2022-06-16 18:16:43 +0545

    scraper implementation

---
+++ scraper.py
@@ -0,0 +1,7 @@
+from bing_image_downloader import  downloader
+import os
+import matplotlib
+impor n
+downloader.download("pretty sunflower", limit=30, output_dir='data/images', adult_filter_off=True)
+
+# Preprocessing
